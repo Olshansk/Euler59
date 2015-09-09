2c4567ca6ee113763965f8522f1d93767a7a580b
Daniel Olshansky <dolshansky@twitter.com>
2015-09-08 21:26:52 -0700

    Some cleanup of code and old vars

--- solution.py
+++ solution.py
@@ -17,39 +17,31 @@ def increment_and_wrap_key(curr_key, min_val, max_val):
 t0 = time.time()
 
 cipher_text_file = 'p059_cipher.txt'
-encryped_chars = [int(i) for i in open(cipher_text_file, 'r').read().split(',')]
+cipher_text = [int(i) for i in open(cipher_text_file, 'r').read().split(',')]
 
 # top 10 most common according to https://en.wikipedia.org/wiki/Most_common_words_in_English
-english_words = set(['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have'])
+common_words = set(['the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have'])
 
 ascii_space = 32
 ascii_a = 97
 ascii_z = 122
 
-decrypted_message = []
 key = [ascii_a, ascii_a, ascii_a]
 
-ki = 0
-last_i = 0
-i = 0
-num_valid_words = 0
-char_regex = re.compile('[^a-zA-Z]')
-num_encrypted_chars = len(encryped_chars)
+num_encrypted_chars = len(cipher_text)
 decrypted_message = [0 for x in xrange(num_encrypted_chars)]
 
 while True:
 
   for x in xrange(num_encrypted_chars):
-    decrypted_message[x] = chr(encryped_chars[x] ^ key[x % 3])
+    decrypted_message[x] = chr(cipher_text[x] ^ key[x % 3])
+
 
   concated_message = ''.join(decrypted_message)
   
   words = [word.strip().lower() for word in concated_message.split(' ')]
-  
-  counter = collections.Counter(words)
-  top_five = counter.keys()
 
-  if (len(set(top_five).intersection(english_words)) > 5):
+  if (len(set(words).intersection(common_words)) > 5):
     break
 
   key = increment_and_wrap_key(key, ascii_a, ascii_z + 1)
